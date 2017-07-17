d428a08a893e6dd8f306b8fe438cff3a7185f572
Minthen <mc_segurado@hotmail.com>
2017-07-17 16:30:36 +0100

    Projecto final

---
+++ jquery.js
@@ -0,0 +1,15 @@
+﻿$( document ).ready(function() {
+ 
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(0)
+    .next()
+    .fadeIn(0)
+    .end()
+    .appendTo('#slideshow');
+}, 5000);
+ 
+
+});
